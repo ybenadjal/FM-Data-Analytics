dcba0e85b5106c35094e16e4e03c68e3f671867b
ybenadjal <77130875+ybenadjal@users.noreply.github.com>
2021-01-09 09:00:10 -0500

    Add files via upload
    fixed install.packages bug from batch file

--- help/installpackages.bat
+++ help/installpackages.bat
@@ -1,9 +1,9 @@
 cd C:\InstalledProg\R\R-4.0.3\bin
-Rscript.exe -e install.packages("tidyverse")
-Rscript.exe -e install.packages("readxl")
-Rscript.exe -e install.packages("magrittr")
-Rscript.exe -e install.packages("plotly")
-Rscript.exe -e install.packages("reactable")
-Rscript.exe -e install.packages("shiny")
-Rscript.exe -e install.packages("shinydashboard")
-Rscript.exe -e install.packages("shinyWidgets")
+Rscript.exe -e "install.packages('tidyverse', repos = 'https://cran.rstudio.com/')"
+Rscript.exe -e "install.packages('readxl', repos = 'https://cran.rstudio.com/')"
+Rscript.exe -e "install.packages('magrittr', repos = 'https://cran.rstudio.com/')"
+Rscript.exe -e "install.packages('plotly', repos = 'https://cran.rstudio.com/')"
+Rscript.exe -e "install.packages('reactable', repos = 'https://cran.rstudio.com/')"
+Rscript.exe -e "install.packages('shiny', repos = 'https://cran.rstudio.com/')"
+Rscript.exe -e "install.packages('shinydashboard', repos = 'https://cran.rstudio.com/')"
+Rscript.exe -e "install.packages('shinyWidgets', repos = 'https://cran.rstudio.com/')"
