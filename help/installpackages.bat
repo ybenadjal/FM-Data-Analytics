ef671dc65262e40fc8dcb0231dbaa5e11ca1e995
ybenadjal <77130875+ybenadjal@users.noreply.github.com>
2021-01-14 23:36:18 -0500

    Delete installpackages.bat
    * app now bypasses installpackages.bat as it checks for missing required packages and install them before running the app

--- help/installpackages.bat
+++
@@ -1,9 +0,0 @@
-cd C:\InstalledProg\R\R-4.0.3\bin
-Rscript.exe -e "install.packages('tidyverse', repos = 'https://cran.rstudio.com/')"
-Rscript.exe -e "install.packages('readxl', repos = 'https://cran.rstudio.com/')"
-Rscript.exe -e "install.packages('magrittr', repos = 'https://cran.rstudio.com/')"
-Rscript.exe -e "install.packages('plotly', repos = 'https://cran.rstudio.com/')"
-Rscript.exe -e "install.packages('reactable', repos = 'https://cran.rstudio.com/')"
-Rscript.exe -e "install.packages('shiny', repos = 'https://cran.rstudio.com/')"
-Rscript.exe -e "install.packages('shinydashboard', repos = 'https://cran.rstudio.com/')"
-Rscript.exe -e "install.packages('shinyWidgets', repos = 'https://cran.rstudio.com/')"
