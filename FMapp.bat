507bb550ab707eb349dc3ba4cac829c49ecb4500
ybenadjal <77130875+ybenadjal@users.noreply.github.com>
2021-01-15 20:49:21 -0500

    Update FMapp.bat

--- FMapp.bat
+++ FMapp.bat
@@ -6,14 +6,14 @@
 :: Rscript partial paths
 echo Setting Rscript.exe's path...
 echo.
-set "r_path1=C:\InstalledProg\R\R-4.0.3\bin"
-set "r_path2=\Rscript.exe"
+set r_path1="C:\InstalledProg\R\R-4.0.3\bin"
+set r_path2="\Rscript.exe"
 
 :: R application partial paths
 echo Setting application's path...
 echo.
-set "app_path1=%cd%"
-set "app_path2=\app.R"
+set app_path1="%cd%"
+set app_path2="\app.R"
 
 :: Run application using Rscript.exe full path from command prompt
 echo Running application...
